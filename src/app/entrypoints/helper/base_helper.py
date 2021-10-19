e811bf955e872487385676e5f3394994d4a437b6
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-19 22:32:24 +0900

    add calc offset

--- src/app/entrypoints/helper/base_helper.py
+++ src/app/entrypoints/helper/base_helper.py
@@ -4,3 +4,7 @@ def calc_available_page(num: int, hit_num: int) -> int:
     if hit_num % num == 0:
         return hit_num // num
     return hit_num // num + 1
+
+
+def calc_offset(page: int, num: int) -> int:
+    return num * (page - 1)
