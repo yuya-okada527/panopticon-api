bffccc73767eb8c52d76079f5ea48f9c5522d78c
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-21 23:26:55 +0900

    fix openapi spe

--- src/app/entrypoints/messages/task_message.py
+++ src/app/entrypoints/messages/task_message.py
@@ -1,7 +1,8 @@
 from typing import List
 
+from domain.models.task_model import Task
 from entrypoints.messages.base_message import SearchResponse
 
 
 class SearchTasksResponse(SearchResponse):
-    results: List
+    results: List[Task]
