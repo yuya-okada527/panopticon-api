88c6798c327a32d7c20de310b6711c0e64252848
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-23 15:02:59 +0900

    remove not beautiful comments

--- src/app/main.py
+++ src/app/main.py
@@ -6,12 +6,8 @@ app = FastAPI(
     title="Panopticon API", description="セルフモニタリングツールPanopticonのためのAPI", version="v1"
 )
 
-##################
 # ルータ定義
-##################
 app.include_router(task_router.router)
 
-##################
 # ミドルウェア定義
-##################
 app.add_middleware(**CORS)
