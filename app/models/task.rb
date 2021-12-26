b567fc4c72a38a8957a534b17583cbd3cb384d60
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-26 22:37:37 +0900

    fix belongs to

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   # Relation
   belongs_to :project
-  belongs_to :task_provider
+  belongs_to :task_provider, optional: true
   has_many :task_status_histories, dependent: :destroy
 end
