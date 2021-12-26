3c2e539a7e5d13eb953bcbd6a26a983cfcde13cc
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-26 21:46:59 +0900

    add task provider

---
+++ app/models/task_provider.rb
@@ -0,0 +1,2 @@
+class TaskProvider < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,21 @@
 # TODO: データの交換方法は変える
 # データ初期化(暫定対応)
 Project.destroy_all
+TaskProvider.destroy_all
 Task.destroy_all
 TaskStatusHistory.destroy_all
 ActiveRecord::Base.connection.execute('ALTER TABLE projects AUTO_INCREMENT = 1')
+ActiveRecord::Base.connection.execute('ALTER TABLE task_providers AUTO_INCREMENT = 1')
 ActiveRecord::Base.connection.execute('ALTER TABLE tasks AUTO_INCREMENT = 1')
 ActiveRecord::Base.connection.execute('ALTER TABLE task_status_histories AUTO_INCREMENT = 1')
 
 # プロジェクト
 project = Project.new(name: "Local Project")
 project.save!
+
+# タスクプロバイダ
+task_provider = TaskProvider.new(project_id: project.id, name: "Panopticon API", provider_kind: 1, provider_url: "https://github.com/")
+task_provider.save!
 
 # タスク
 [
