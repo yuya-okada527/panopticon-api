dbf8441ccdd6612c6f949f7922af7fe1dcc62784
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-27 17:00:23 +0900

    add task providers post api

--- app/controllers/v1/task_providers_controller.rb
+++ app/controllers/v1/task_providers_controller.rb
@@ -4,4 +4,15 @@ class V1::TaskProvidersController < ApplicationController
     @task_providers = TaskProvider.by_project_id(params[:project_id])
     render :template => 'task_providers_get.json.jb'
   end
+
+  def task_providers_post
+    task_provider = TaskProvider.new(
+      project_id: params[:project_id],
+      name: params.require(:name),
+      provider_kind: params.require(:provider_kind),
+      provider_url: params[:provider_url] || ""
+    )
+    task_provider.save!
+    render :template => 'task_providers_post.json.jb', :locals => { :id => task_provider.id }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # ProjectsAPI
+  # Projects API
   get '/v1/projects', to: 'v1/projects#projects_get'
 
-  # TasksAPI
+  # Tasks API
   get '/v1/projects/:project_id/tasks', to: 'v1/tasks#tasks_get'
   post '/v1/projects/:project_id/tasks', to:'v1/tasks#tasks_post'
   get '/v1/projects/:project_id/tasks/:task_id', to: 'v1/tasks#tasks_task_id_get'
   put '/v1/projects/:project_id/tasks/:task_id', to: 'v1/tasks#tasks_task_id_put'
   put '/v1/projects/:project_id/tasks/:task_id/status', to: 'v1/tasks#tasks_task_id_status_put'
 
-  # TaskProvidersAPI
+  # TaskProviders API
   get '/v1/projects/:project_id/task_providers', to: 'v1/task_providers#task_providers_get'
+  post '/v1/projects/:project_id/task_providers', to: 'v1/task_providers#task_providers_post'
 end
