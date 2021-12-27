8f234d3161c9ee1df594affc45d12047d903d210
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-27 13:09:39 +0900

    add post task api

--- app/controllers/v1/tasks_controller.rb
+++ app/controllers/v1/tasks_controller.rb
@@ -1,14 +1,20 @@
 class V1::TasksController < ApplicationController
 
   def tasks_get
-    project_id = params[:project_id]
-    status = params[:status]
-    keyword = params[:keyword]
     @tasks = Task
-      .by_project_id(project_id)
-      .by_status(status)
-      .by_keyword(keyword)
+      .by_project_id(params[:project_id])
+      .by_status(params[:status])
+      .by_keyword(params[:keyword])
     render :template => 'tasks_get.json.jb'
+  end
+
+  def tasks_post
+    name = params.require(:name)
+    description = params.require(:description)
+    project_id = params.require(:project_id)
+    task = Task.new(name: name, description: description, project_id: project_id)
+    task.save!
+     render :template => 'tasks_post.json.jb', :locals => { :id => task.id }
   end
 
   def tasks_task_id_get
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/v1/projects', to: 'v1/projects#projects_get'
   get '/v1/projects/:project_id/tasks', to: 'v1/tasks#tasks_get'
+  post '/v1/projects/:project_id/tasks', to:'v1/tasks#tasks_post'
   get '/v1/projects/:project_id/tasks/:task_id', to: 'v1/tasks#tasks_task_id_get'
 end
