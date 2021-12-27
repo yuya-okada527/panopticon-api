a525464c265a3091972f938658e583b23b1c109f
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-27 10:25:13 +0900

    fix routes

--- app/controllers/v1/projects_controller.rb
+++ app/controllers/v1/projects_controller.rb
@@ -1,4 +1,4 @@
-class ProjectsController < ApplicationController
+class V1::ProjectsController < ApplicationController
   def index
     @projects = Project.all
     render :template => 'projects_index.json.jb'
