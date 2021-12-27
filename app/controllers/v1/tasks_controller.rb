ab3abe88e31753a5dbfb13fb66c68f815e3af815
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-27 12:45:22 +0900

    add tasks task id get api

--- app/controllers/v1/tasks_controller.rb
+++ app/controllers/v1/tasks_controller.rb
@@ -3,10 +3,19 @@ class V1::TasksController < ApplicationController
   def tasks_get
     project_id = params[:project_id]
     status = params[:status]
+    # TODO name
     @tasks = Task
       .by_project_id(project_id)
       .by_status(status)
     render :template => 'tasks_get.json.jb'
+  end
+
+  def tasks_task_id_get
+    @task = Task
+      .by_task_id(params[:task_id])
+      .by_project_id(params[:project_id])
+      .first
+    render :template => 'tasks_task_id_get.json.jb'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/v1/projects', to: 'v1/projects#projects_get'
   get '/v1/projects/:project_id/tasks', to: 'v1/tasks#tasks_get'
+  get '/v1/projects/:project_id/tasks/:task_id', to: 'v1/tasks#tasks_task_id_get'
 end
