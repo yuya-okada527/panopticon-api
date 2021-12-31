6c616ab4fbc952a0d90206d400564979de42c953
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-31 12:17:45 +0900

    add cors setting

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module Api
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :put, :delete]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
